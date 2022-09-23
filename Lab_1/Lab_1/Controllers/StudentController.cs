02f869788e56d80454a2b32b3b751f7c85f68ea9
mahimtalukder <19-41621-3@student.aiub.edu>
2022-09-23 16:07:13 +0600

    Add lab task 1

---
+++ Lab_1/Lab_1/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var age = 20;
+            dynamic name = "Mahim";
+            ViewBag.name = name;
+            ViewData["age"] = age;
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            //return Redirect("");
+            return RedirectToAction("About", "Home");
+        }
+    }
+}
