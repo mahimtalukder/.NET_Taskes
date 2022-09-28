f2d53d5ab47074448b4250ed6d4063c0bb813c71
mahimtalukder <19-41621-3@student.aiub.edu>
2022-09-28 13:16:08 +0600

    Lab 2

---
+++ Lab_2/Lab_2/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab_2.Models
+{
+    public class User
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Special character not allowed")]
+        [MaxLength(35, ErrorMessage = "Maximum length 35")]
+        [MinLength(3, ErrorMessage = "Minimum length 3")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string Phone { get; set; } 
+
+        public string DOB { get; set; }
+
+        public string Blood_group { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Password { get; set; }
+
+        public string Confirm_password { get; set; }
+
+    }
+}
