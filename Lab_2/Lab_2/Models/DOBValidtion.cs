d8355074c3707720e53a55c95304d4439dbbc494
mahimtalukder <19-41621-3@student.aiub.edu>
2022-10-01 10:43:55 +0600

    add final lab 2

---
+++ Lab_2/Lab_2/Models/DOBValidtion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab_2.Models
+{
+    public class DOBValidtion
+    {
+
+        public class DOBDateValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                DateTime date;
+                bool parsed = DateTime.TryParse(value.ToString(), out date);
+                if (!parsed)
+                    return new ValidationResult("Invalid Date");
+                else
+                {
+                    //change below as per requirement
+                    var min = DateTime.Now.AddYears(-18); //for min 18 age
+                    var msg = string.Format("Minimum age limite is 18", min);
+                    try
+                    {
+                        if (date > min)
+                            return new ValidationResult(msg);
+                        else
+                            return ValidationResult.Success;
+                    }
+                    catch (Exception e)
+                    {
+                        return new ValidationResult(e.Message);
+                    }
+                }
+            }
+        }
+    }
+}
